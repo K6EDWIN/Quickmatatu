fb9a40619716eb80cd15511b1b8f4e1db0877d4e
Edwin kamau <166378939+K6EDWIN@users.noreply.github.com>
2025-08-26 15:36:40 +0300

    platform update

--- App.js
+++ App.js
@@ -9,11 +9,47 @@ import DriverHomepage from './Components/DriverHomepage';
 import RealTimeTrackingScreen from "./Components/RealTimeTrackingScreen";
 import RatingPage from './Components/RatingPage';
 import AccountScreen from './Components/AccountScreen';
-import HistoryScreen from './Components/HistoryScreen';
+import HistoryScreenDriver from './Components/HistoryScreenDriver';
+import HistoryScreenUser from './Components/HistoryScreenUser';
+import TicketsPage from './Components/TicketsPage';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  // const [userType, setUserType] = useState('');
+  // const [showLogin, setShowLogin] = useState(false);
+  // const [loggedIn, setLoggedIn] = useState(false);
+
+  // const handleLoginSuccess = () => {
+  //   setLoggedIn(true);
+  //   console.log(loggedIn) // Set loggedIn to true when login is successful
+  // };
+
+  // console.log(loggedIn) 
+  // return (
+  //   <>
+  //     {loggedIn ? (
+  //       // Show DriverHomepage only when loggedIn is true
+       
+
+  //       <DriverHomepage />
+  //     ) : (
+  //       // Show either the Login or Registration form
+  //       showLogin ? (
+  //         <LoginForm 
+  //           handleLoginSuccess={handleLoginSuccess}
+  //           onCreateAccountPress={() => {
+  //             setShowLogin(false);
+  //             //setLoggedIn(true);
+  //           }}
+  //            // Pass the success handler to LoginForm
+  //         />
+  //       ) : (
+  //         <RegistrationForm onLoginPress={() => setShowLogin(true)} userType={userType} setUserType={setUserType}/>
+  //       )
+  //     )}
+  //   </>
+  // );
   return(
     <>
     <NavigationContainer>
@@ -24,7 +60,9 @@ const App = () => {
         <Stack.Screen name="DriverHomepage" component={DriverHomepage} />
         <Stack.Screen name="RatingPage" component={RatingPage} />
         <Stack.Screen name="UserAccount" component={AccountScreen} />
-        <Stack.Screen name="HistoryScreen" component={HistoryScreen} />
+        <Stack.Screen name="HistoryScreenDriver" component={HistoryScreenDriver} />
+        <Stack.Screen name="HistoryScreenUser" component={HistoryScreenUser} />
+        <Stack.Screen name="TicketsPage" component={TicketsPage} />
       </Stack.Navigator>
     </NavigationContainer>
     </>
---
+++ Components/RenderHomePage.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import { View, Text } from 'react-native'
+import { UserTypeContext } from '../UserTypeContext';
+
+function RenderHomePage({userType}) {
+
+    console.log(userType)
+  return (
+    <View>
+    <Text>sdgvregettgbettebtrb</Text>
+    </View>
+  )
+}
+
+export default RenderHomePage
